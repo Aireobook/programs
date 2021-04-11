08e5c3705a21a85540da385d72e157ea62de2d3a
antenea <paul.edgard.asgarian@gmail.com>
2021-04-11 11:39:09 +0200

    3a

--- ajax/ajax.js
+++ ajax/ajax.js
@@ -1,57 +1,83 @@
 
+
+document.addEventListener('DOMContentLoaded', () => {
+    InitAction();
+})
+
+
 function InitAction(){
     if ( !(this instanceof InitAction)){
         return new InitAction();
     }
+    let kolor = 'yellow';
+    let parameter_url = this.getUrlParameters() || false;
 
     let _old = false;
     let _oldColor = false;
-
-    // this.link()
-
-    let url = 'pages/home.html'
+    let url = ''
     let self = this;
+    const hostname =  window.location.pathname;
+
+    if (parameter_url){
+        url = 'pages/' +  parameter_url + '.html';
+        window.addEventListener('load', function (){
+            htmlLink(url);
+        }, false)
+    }
+
+
+    function htmlLink(url){
+        self.ajax(url);
+
+        let url_par = self.getUrlParameters('page');
+        let data_links = document.querySelectorAll('[data-link]')
+
+        for (let data_link of data_links){
+            let data = data_link.dataset.link;
+            if (url_par === data){
+                data_link.style.color = 'red';
+                _old = data_link;
+            }
+        }
+    }
 
     let links = document.querySelectorAll('.js-link');
 
     for (let link of links){
-
         link.addEventListener('click', jsLinken, false)
-
-
         function jsLinken(e){
             e.preventDefault();
-            console.log('linkkkk',link.getAttribute('href'))
-
             let ala = link.getAttribute('href')
+
+            let button_link = link.dataset.link;
+            window.history.replaceState({}, document.title, hostname + '?page=' + button_link);
 
             self.ajax(ala);
             if (_old !== false){
-                _old.style.color = _oldColor;
+                _old.style.color = 'inherit';
             }
 
             _oldColor = link.style.color;
             link.style.color = 'yellow'
             _old = link;
-
         }
-
     }
-
-
-
-
 
 }
 
 
+
+InitAction.prototype.getUrlParameters = function (paramewter = 'page'){
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    return  urlParams.get(paramewter)
+}
 
 InitAction.prototype.ajax = function (url){
     if (url ){
         fetch(url)
             .then(res => res.text())
             .then(html => {
-                // this.obietnica(JSON.parseparse(html))
                 this.obietnica(html)
             }).catch(err => {
             console.warn('Something went wrong.', err)
@@ -60,42 +86,8 @@ InitAction.prototype.ajax = function (url){
 }
 
 
-
-
 InitAction.prototype.obietnica = function (html){
-    console.log('html', html)
-
     let action = document.getElementById('action')
-
     action.innerHTML = html
-
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-
-    InitAction();
-})
-
-InitAction.prototype.link = function (){
-    let url = 'pages/home.html'
-    let self = this;
-
-    let links = document.querySelectorAll('.js-link');
-
-    for (let link of links){
-
-        link.addEventListener('click', jsLinken, false)
-
-
-        function jsLinken(e){
-            e.preventDefault();
-            console.log('linkkkk',link)
-
-            self.ajax(url);
-
-        }
-
-    }
-
-
-}
--- nav/nav.js
+++ nav/nav.js
@@ -1,16 +1,10 @@
 
 window.addEventListener('DOMContentLoaded', function () {
-
-
-
-
     Animation({
         "menu_li": "menu-li",
         "collapsible_a": "collapsible-a",
         "collapsible_body": "collapsible-body",
-
     });
-
 })
 
 
@@ -20,75 +14,91 @@ window.addEventListener('DOMContentLoaded', function () {
      }
 
 
+     // 1. Paramentr page w adresie url
+     let parm_url = this.parameter();
+    console.log('param_url', parm_url)
+     if (parm_url){
+         let data_link_url =  this.urlSearchLink(parm_url);   // a
 
-
-      this._config = this._mixOptions(param);
-
-     let self = this;
-     let menus_li = document.querySelectorAll('.'+ this._config['menu_li'])
-
-     for (let menu_li of menus_li ){
-         let _h_li = false;
-         let _h_div = false;
-         let _el_div = false;
-         let colabse_a = menu_li.querySelector('.' + this._config['collapsible_a'])
-
-         colabse_a.addEventListener('click', Collapsen, false);
-
-         function Collapsen(e){
-
-             let el_li  = e.target.parentNode;  // li
-             let el_a   = e.target;             // a
-             if (_el_div === false){
-                  _el_div = e.target.closest(".menu-li").querySelector('.' + self._config['collapsible_body']);  // div
-             }
-
-             if (_h_li === false){
-                _h_li = parseInt(getComputedStyle(el_li).height);
-             }
-
-             if (menu_li.classList.contains('active')){
-                     self.zwin.call(this, 0, _h_div, _el_div, self._config['skok']);
-                     el_li.style.height = _h_li;
-                     menu_li.classList.remove('active')
-             }else{
-                    el_li.style.overflow = 'hidden';
-                    el_li.style.height = _h_li + 'px';
-                    _el_div.style.overflow = 'hidden';
-                    _el_div.style.display = 'block';
-                    if (_h_div === false){
-                        _h_div = parseInt(getComputedStyle(_el_div).height);
-                    }
-                    _el_div.style.height = '0';
-                    el_li.style.height = 'inherit';
-                    el_li.style.overflow = 'visible';
-
-                    self.rozwin.call(this, 0, _h_div, _el_div, self._config['skok']);
-                    menu_li.classList.add('active');
-             }
-
-         }
+         this.open(data_link_url);
      }
+
+    this.navVoyager();
+
  }
 
 
-Animation.prototype._mixOptions = function (config){
 
 
-    let defaultConfig = JSON.parse(JSON.stringify(this._default));
+Animation.prototype.navVoyager = function (){
 
-    for (const key in defaultConfig){
-        if (defaultConfig.hasOwnProperty(key)){
-            if ( key in config){
-                defaultConfig[key] = config[key]
-            }
+    let self = this;
+    let a_menus = document.querySelectorAll('.collapsible-a')
+
+    for (let a_menu of a_menus){
+        a_menu.addEventListener('click', Voyager, false);
+
+        function Voyager(e){
+           if (a_menu.classList.contains('active')){
+               self.close(a_menu)
+           }else{
+               self.open(a_menu)
+           }
         }
-
     }
-    return defaultConfig;
 }
 
-Animation.prototype.rozwin = function (_min, _max, el_div, skok = 10){
+
+
+Animation.prototype.open = function (a_link){
+
+    // if (a_link === null) {
+    //     console.log("Error: not a_link!")
+    //     return false;
+    // }
+
+    let li = a_link.closest('.menu-li');
+
+    if (li === null) {
+        console.log("Not paret element li !")
+        return false;
+    }
+
+    let div = li.querySelector('.collapsible-body')
+
+    let ul_height_start = getComputedStyle(li).height;
+    let al = li.querySelector('.collapsible-a')
+
+    li.classList.add('active')
+    al.classList.add('active')
+
+    li.style.overflow = "hidden";       // 1. zatrzymać rozwijanie li
+    li.style.height = ul_height_start;  // 2. zablokować wysokoc li
+    div.style.display = 'block';
+    const  div_height_full = parseInt(getComputedStyle(div).height); // 3.  obliczona wysokość div przy rozwinięciu
+    li.style.height = 'inherit';
+    li.style.overflow = "inherit";
+     this.rozwin(0, div_height_full, div)
+}
+
+
+
+
+Animation.prototype.close = function (a_link){
+
+    let li  = a_link.closest('.menu-li');
+    let div = li.querySelector('.collapsible-body')
+    let div_height = parseInt(getComputedStyle(div).height);
+
+    li.classList.remove('active')
+    a_link.classList.remove('active')
+
+    this.zwin(0, div_height, div )
+}
+
+
+
+Animation.prototype.rozwin = function (_min, _max, el_div, skok = 5){
 
     let i = 0;
     const i_max = Math.ceil(_max/skok);
@@ -114,19 +124,23 @@ Animation.prototype.rozwin = function (_min, _max, el_div, skok = 10){
 
 }
 
-Animation.prototype.zwin = function (_min, _max, el_div, skok = 10){
+Animation.prototype.zwin = function (_min, _max, el_div, skok = 5){
 
     let i = 0;
     const i_max = Math.ceil(_max/skok);
     let n = _min;
     amr();
     function amr(){
+        // console.log('i', i)
         el_div.style.height = (_max - n) + 'px'
         n += skok;
         i++;
 
         if (i === i_max){
-            el_div.style.height = _min + 'px'
+            // el_div.style.height = _min + 'px'
+            el_div.style.display = 'none';
+            el_div.style.height = null ;
+
         }else{
             if(n <= _max ){
                 setTimeout(function (){
@@ -139,10 +153,106 @@ Animation.prototype.zwin = function (_min, _max, el_div, skok = 10){
 
 }
 
-Animation.prototype._default = {
-    "menu_li": "menu-li",
-    "collapsible_a": "collapsible-a",
-    "collapsible_body": "collapsible-body",
-    "skok": 10
+
+
+
+Animation.prototype.urlSearchLink = function (parm_url){
+    return   document.querySelector('[data-link*="' + parm_url +'" ]')
 }
 
+
+
+
+
+ //
+ // Animation.prototype.openActiveMenuLink = function (a_open_menu, ul_height_start){
+ //
+ //     let div_heint_full = false;
+ //
+ //     let li = a_open_menu.closest('.menu-li')
+ //    let a = a_open_menu;
+ //    let div = li.querySelector('.collapsible-body');
+ //    div.style.display = "block"
+ //
+ //   if ( li.classList.contains('active')){
+ //
+ //       li.classList.remove('active')
+ //         this.zwin(0, ul_height_start, div)
+ //       // div.style.display = "none"
+ //
+ //   } else{
+ //       // div.style.display = "block"
+ //       li.classList.add('active')
+ //       li.style.overflow = "hidden";
+ //       li.style.height = ul_height_start;
+ //       div.style.display = 'block';
+ //
+ //       if (div_heint_full === false){
+ //           div_heint_full = parseInt(getComputedStyle(div).height);
+ //       }
+ //
+ //       console.log('div_height_full', div_heint_full)
+ //
+ //       this.rozwin(0, div_heint_full, div)
+ //   }
+ //
+ //
+ //
+ //
+ // }
+ //
+
+
+// Animation.prototype.ulFull =  function (){
+//
+// }
+
+
+
+ // // 1. Jeśli instnie parametr page w adresie url - otwiramy nav
+ // Animation.prototype.otworzMenuParam = function (parm_url){
+ //     // znajdujemy link w navigacji o data-likk = param_url
+ //     return  data_link = document.querySelector('[data-link*="' + parm_url +'" ]')
+ // }
+
+
+Animation.prototype.getUrlParameters = function (paramewter = 'page'){
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+   return urlParams.get(paramewter)
+}
+
+
+Animation.prototype.parameter = function (){
+   return  parameter_url = this.getUrlParameters('page') || false;
+}
+
+
+
+
+//
+//
+// Animation.prototype._mixOptions = function (config){
+//
+//
+//     let defaultConfig = JSON.parse(JSON.stringify(this._default));
+//
+//     for (const key in defaultConfig){
+//         if (defaultConfig.hasOwnProperty(key)){
+//             if ( key in config){
+//                 defaultConfig[key] = config[key]
+//             }
+//         }
+//
+//     }
+//     return defaultConfig;
+// }
+
+//
+// Animation.prototype._default = {
+//     "menu_li": "menu-li",
+//     "collapsible_a": "collapsible-a",
+//     "collapsible_body": "collapsible-body",
+//     "skok": 10
+// }
+//
