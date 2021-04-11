460cc24ff4cd2413ff4b8c02377268333459f5e4
antenea <paul.edgard.asgarian@gmail.com>
2021-04-11 13:25:00 +0200

    3c

--- public/js/programs.js
+++ public/js/programs.js
@@ -3,7 +3,7 @@ window.addEventListener('load', function () {
 
     let rolling = document.getElementById('rolling');
     let vertican_nav = document.querySelector('.vertical-nav');
-
+    let layout = vertican_nav.closest('.layout');
 
 
 
@@ -12,10 +12,12 @@ window.addEventListener('load', function () {
 
     function rolLingn(e) {
         if (vertican_nav.classList.contains('hidden')){
-            vertican_nav.classList.remove('hidden')
+            vertican_nav.classList.remove('hidden');
+            layout.classList.remove('hidden');
             vertican_nav.style.marginLeft = '0'
         }else{
             vertican_nav.classList.add('hidden')
+            layout.classList.add('hidden')
             vertican_nav.style.marginLeft = '-290px'
         }
     }
